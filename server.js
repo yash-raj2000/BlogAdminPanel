8756396a85e6fdc118ab7f2b77885c4daa028c80
Yash Raj <yashrajsingh282@gmail.com>
2024-03-19 12:16:30 +0530

    made some changes in server.js

--- server.js
+++ server.js
@@ -8,22 +8,22 @@ const pool = require("./database");
 app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("Server up and running..!");
-  console.log("server is running");
 });
 
 app.post("/postBlog", async (req, res) => {
   try {
     const { date, title, category, content } = req.body;
-      const result = await pool.query("INSERT INTO blogdata (date, title, category, content) VALUES ($1, $2, $3, $4) RETURNING *",
-          [date, title, category, content]
-      );
-      res.json(result.rows[0]);
+    const result = await pool.query(
+      "INSERT INTO blogdata (date, title, category, content) VALUES ($1, $2, $3, $4) RETURNING *",
+      [date, title, category, content]
+    );
+    res.json(result.rows[0]);
   } catch (error) {
-      console.error(error.message);
+    console.error(error.message);
   }
 });
 
